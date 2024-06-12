8592eb27464d5d6ef3c04ebe2af1a7357ca96047
Marco <marcoignacio.9637@gmail.com>
2024-06-12 09:45:07 -0400

    fix:store rendicion

--- app/Http/Controllers/Rendicion/RendicionController.php
+++ app/Http/Controllers/Rendicion/RendicionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Rendicion;
 use App\Events\ProcesoRendicionGastoCreated;
 use App\Events\ProcesoRendicionGastoStatus;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProcesoRendicion\StoreProcesoRendicionRequest;
 use App\Http\Requests\Rendicion\AnularRendicionRequest;
 use App\Http\Requests\Rendicion\AprobarRendicionRequest;
 use App\Http\Requests\Rendicion\StoreRendicionRequest;
@@ -146,13 +147,159 @@ class RendicionController extends Controller
         }
     }
 
+    private function transformData($data)
+    {
+        array_walk_recursive($data, function (&$value) {
+
+            if (is_string($value)) {
+                $decodedValue = json_decode($value, true);
+                if ($decodedValue !== null) {
+                    $value = $decodedValue;
+                }
+            }
+        });
+
+        if (!isset($data['observacion'])) {
+            $data['observacion'] = null;
+        }
+
+        if (isset($data['archivos']) && !empty($data['archivos'])) {
+            array_walk_recursive($data, function (&$value) {
+                if (is_string($value)) {
+                    $decodedValue = json_decode($value, true);
+                    if ($decodedValue !== null) {
+                        $value = $decodedValue;
+                    }
+                }
+            });
+
+            if (isset($data['actividades']) && !empty($data['actividades'])) {
+                foreach ($data['actividades'] as $index => $value) {
+                    if ($value === null) {
+                        $data['actividades'][$index] = [];
+                    }
+                }
+            }
+        }
+        return $data;
+    }
+
     public function storeRendicion(Request $request)
+    {
+        try {
+
+            $transformedData = $this->transformData($request->all());
+
+            $storeProcesoRendicionRequest = new StoreProcesoRendicionRequest();
+            $storeProcesoRendicionRequest->merge($transformedData);
+            $storeProcesoRendicionRequest->solicitud_uuid = $transformedData['solicitud_uuid'];
+
+            $validator = Validator::make(
+                $transformedData,
+                $storeProcesoRendicionRequest->rules(),
+                $storeProcesoRendicionRequest->messages(),
+                $storeProcesoRendicionRequest->attributes()
+            );
+
+            $storeProcesoRendicionRequest->withValidator($validator);
+
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()], 422);
+            }
+
+            $validatedData = $validator->validated();
+
+            $solicitud = Solicitud::where('uuid', $validatedData['solicitud_uuid'])->firstOrFail();
+
+            if ($solicitud) {
+                $actividades = [];
+                $observacion = null;
+                if (isset($validatedData['observacion'])) {
+                    $observacion = $validatedData['observacion'];
+                }
+                $proceso_rendicion_gasto = ProcesoRendicionGasto::create([
+                    'solicitud_id'   => $solicitud->id,
+                    'observacion'    => $observacion
+                ]);
+                if ($proceso_rendicion_gasto) {
+                    if ($validatedData['actividades']) {
+                        foreach ($validatedData['actividades'] as $actividad) {
+                            $actividad_id               = (int)$actividad['id'];
+                            $rinde_gasto                = (bool)$actividad['rinde_gasto'];
+                            $mount                      = (int)$actividad['mount'];
+                            $rinde_gastos_servicio      = (bool)$actividad['rinde_gastos_servicio'];
+
+                            $actividades[] = [
+                                'rinde_gasto'                   => $rinde_gasto,
+                                'mount'                         => $rinde_gasto ? $mount : null,
+                                'rinde_gastos_servicio'         => $rinde_gastos_servicio,
+                                'proceso_rendicion_gasto_id'    => $proceso_rendicion_gasto->id,
+                                'actividad_gasto_id'            => $actividad_id
+                            ];
+                        }
+                    }
+                    $proceso_rendicion_gasto->addRendiciones($actividades);
+
+                    if ($validatedData['archivos']) {
+                        foreach ($validatedData['archivos'] as $file) {
+                            $fecha_solicitud    = Carbon::parse($solicitud->fecha_inicio);
+                            $year               = $fecha_solicitud->format('Y');
+                            $month              = $fecha_solicitud->format('m');
+                            $fileName           = 'rendicion_de_gastos/' . $solicitud->funcionario->rut . '/' . $year . '/' . $month . '/' . $file->getClientOriginalName();
+                            $path               = Storage::disk('public')->putFileAs('archivos', $file, $fileName);
+
+                            $store = Documento::create([
+                                'url'           => $path,
+                                'nombre'        => $file->getClientOriginalName(),
+                                'size'          => $file->getSize(),
+                                'format'        => $file->getMimeType(),
+                                'extension'     => $file->getClientOriginalExtension(),
+                                'is_valid'      => $file->isValid(),
+                                'solicitud_id'  => $solicitud->id,
+                                'proceso_rendicion_gasto_id'    => $proceso_rendicion_gasto->id,
+                                'user_id'       => $solicitud->user_id,
+                                'model'         => Documento::MODEL_RENDICION
+                            ]);
+                        }
+                    }
+
+                    $proceso_rendicion_gasto = $proceso_rendicion_gasto->fresh();
+
+                    if ($proceso_rendicion_gasto) {
+                        $firma_disponible = $this->isFirmaDisponibleActionPolicy($proceso_rendicion_gasto, null);
+                        $estado = [
+                            'status'                => EstadoProcesoRendicionGasto::STATUS_INGRESADA,
+                            'p_rendicion_gasto_id'  => $proceso_rendicion_gasto->id,
+                            'role_id'               => $firma_disponible->is_firma ? $firma_disponible->firma->role_id : null,
+                            'posicion_firma'        => $firma_disponible->is_firma ? $firma_disponible->firma->posicion_firma : null,
+                            'is_subrogante'         => $firma_disponible->is_subrogante
+                        ];
+                        $status = EstadoProcesoRendicionGasto::create($estado);
+                        ProcesoRendicionGastoCreated::dispatch($proceso_rendicion_gasto);
+                    }
+
+                    return response()->json(
+                        array(
+                            'status'        => 'success',
+                            'title'         => "Rendición de gastos ingresada con éxito",
+                            'message'       => null,
+                            'data'          => ProcesoRendicionGastoResource::make($proceso_rendicion_gasto)
+                        )
+                    );
+                }
+            }
+        } catch (\Exception $error) {
+            return response()->json($error->getMessage());
+        }
+    }
+
+    /* public function storeRendicion(StoreProcesoRendicionRequest $request)
     {
         try {
             $solicitud = Solicitud::where('uuid', $request->solicitud_uuid)->firstOrFail();
             // Obtener todos los datos de la solicitud
             $data = $request->all();
-
+            return $data;
             // Decodificar el campo "medio_transporte" si es una cadena JSON
             array_walk_recursive($data, function (&$value) {
 
@@ -171,6 +318,7 @@ class RendicionController extends Controller
                     }
                 }
             }
+            Log::info($data['archivos']);
             $is_avion = $solicitud->transportes()->where('solicitud_transporte.transporte_id', 1)->exists();
             $validator = Validator::make($data, [
                 'solicitud_uuid'                            => ['required'],
@@ -309,7 +457,7 @@ class RendicionController extends Controller
         } catch (\Exception $error) {
             return response()->json($error->getMessage());
         }
-    }
+    } */
 
     public function updateRendicion(Request $request)
     {
---
+++ app/Http/Requests/ProcesoRendicion/StoreProcesoRendicionRequest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Requests\ProcesoRendicion;
+
+use App\Models\Solicitud;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreProcesoRendicionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $solicitud = Solicitud::where('uuid', $this->solicitud_uuid)->first();
+        return [
+            'solicitud_uuid'                            => ['required'],
+            'actividades'                               => ['present', 'required', 'array'],
+            'actividades.*.id'                          => ['required'],
+            'actividades.*.rinde_gasto'                 => ['required', 'in:0,1'],
+            'actividades.*.mount'                       => ['required_if:actividades.*.rinde_gasto,1'],
+            'actividades.*.rinde_gastos_servicio'       => [
+                function ($attribute, $value, $fail) use ($solicitud) {
+                    $actividades = $this->input('actividades', []);
+                    $is_avion = $solicitud->transportes()->where('solicitud_transporte.transporte_id', 1)->exists();
+
+                    foreach ($actividades as $index => $actividad) {
+                        if ($actividad['id'] == 1 && $actividad['rinde_gastos_servicio'] === null && $is_avion) {
+                            if ($index === 0) {
+                                $fail('Es obligatorio cuando la actividad de transporte es Avión.');
+                            }
+                        }
+                    }
+                },
+            ],
+            'archivos'                                  => ['nullable'],
+            'observacion'                               => [
+                function ($attribute, $value, $fail) {
+                    $actividades = $this->input('actividades', []);
+
+                    foreach ($actividades as $actividad) {
+                        if ($actividad['id'] === 12 && $actividad['rinde_gasto'] === 1 && $actividad['mount'] > 0) {
+                            if (empty($value)) {
+                                $fail('El campo observacion es obligatorio cuando seleccione OTROS.');
+                            }
+                        }
+                    }
+                },
+            ],
+        ];
+    }
+
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $actividades = $this->input('actividades', []);
+            $hasRindeGasto = false;
+
+            foreach ($actividades as $actividad) {
+                if (isset($actividad['rinde_gasto']) && $actividad['rinde_gasto'] == 1) {
+                    $hasRindeGasto = true;
+                    break;
+                }
+            }
+
+            if (!$hasRindeGasto) {
+                $validator->errors()->add('actividades', 'Debe haber al menos una actividad rendida.');
+            }
+        });
+    }
+
+    public function messages()
+    {
+        return [
+            'actividades.*.rinde_gasto.required'           => 'La :attribute es obligatoria',
+            'actividades.*.mount.required_if'              => 'El :attribute es obligatorio',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'actividades.*.rinde_gasto'     => 'actividad',
+            'actividades.*.mount'            => 'monto',
+        ];
+    }
+
+}
